93c41e0f7e9a4ea9588390087531520f4ec5490c
Oscra Cely <oscarcej@gmail.com>
2020-08-19 16:29:06 -0500

    additional fixes

--- app/src/main/java/com/example/othello/Matchmaking.java
+++ app/src/main/java/com/example/othello/Matchmaking.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,11 +17,13 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 public class Matchmaking extends AppCompatActivity {
 
     private DatabaseReference mDatabase;
+    private DatabaseReference users;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -44,17 +48,42 @@ public class Matchmaking extends AppCompatActivity {
             System.out.println(personId);
 
 
+
             mDatabase = FirebaseDatabase.getInstance().getReference();
-            mDatabase.child("users/" + personId).child("personId").setValue(personId);
-            mDatabase.child("users/" + personId).child("personName").setValue(personName);
-            mDatabase.child("users/" + personId).child("email").setValue(personEmail);
+
+            mDatabase.child("usuarios_disponibles/" + personId).child("personId").setValue(personId);
+            mDatabase.child("usuarios_disponibles/" + personId).child("personName").setValue(personName);
+            mDatabase.child("usuarios_disponibles/" + personId).child("email").setValue(personEmail);
+
+            users = mDatabase.child("usuarios_disponibles");
+            Query usersQuery = users.orderByChild("usuarios_disponibles/");
+            usersQuery.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot snapshot) {
+                    long c = snapshot.getChildrenCount();
+//                    for (DataSnapshot single: snapshot.getChildren()){
+//                        c++;
+//                    }
+                    TextView usuarios_online = findViewById(R.id.usuarios_conectados);
+                    String online = String.format(getResources().getString(R.string.usuarios_conectados), c);
+                    usuarios_online.setText(online);
+
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
+
 
         }
 
         ValueEventListener postListener = new ValueEventListener() {
             public void onDataChange(DataSnapshot mDatabase) {
                 // Get Post object and use the values to update the UI
-                Jugador jugador = mDatabase.child("personName").getValue(Jugador.class);
+
+                Jugador jugador = mDatabase.child("users").getValue(Jugador.class);
                 // ...
             }
             public void onCancelled(DatabaseError databaseError) {
