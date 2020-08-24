ddc9da3adfbf1aaa80e9fdc91f6f8b7ff7d9e01b
Oscra Cely <oscarcej@gmail.com>
2020-08-24 09:07:55 -0500

    validaciones de final

---
+++ app/src/main/java/com/example/othello/Resultado.java
@@ -0,0 +1,42 @@
+package com.example.othello;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.othello.modelo.Jugador;
+import com.example.othello.modelo.Partida;
+import com.example.othello.modelo.Tablero;
+
+public class Resultado extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.resultado_final);
+        int [] fichas = getIntent().getIntArrayExtra("fichas");
+        Jugador yo = (Jugador) getIntent().getSerializableExtra("yo");
+        TextView resultado = findViewById(R.id.resultado);
+        Button btn_inicio = findViewById(R.id.btn_inicio);
+        Button btn_salir = findViewById(R.id.btn_salir);
+
+
+        if (fichas[0] == fichas[1]){
+            resultado.setText("EMPATE");
+            resultado.setTextColor(Color.parseColor("#FF7E39"));
+        } else {
+            int contFichas = (yo.getFicha() == Tablero.NEGRA) ? 1 : 0;
+            if (fichas[contFichas] > 32 ){
+                resultado.setText("GANASTE");
+                resultado.setTextColor(Color.parseColor("#69C33B"));
+            } else {
+                resultado.setText("PERDISTE");
+                resultado.setTextColor(Color.parseColor("#FF5739"));
+            }
+        }
+
+    }
+}
